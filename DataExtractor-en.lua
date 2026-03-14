b7ebb90b7276835695aaa432af84ff453ec84f90
RodMajors <pro-vana@outlook.com>
2026-03-14 19:15:39 +0800

    部件新增具体信息

--- DataExtractor-en.lua
+++ DataExtractor-en.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c341dfd5026738c1152ae7d080d33ff9f9be58bfc6f1e8cf543fab5ad08dfb27
-size 90860868
+oid sha256:959e08bd03d465e2bea70799aeb901381f1ac2553d895af476dfc06c36f1ca6c
+size 113621556
